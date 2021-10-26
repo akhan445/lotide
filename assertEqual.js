cf1fa997be36b275beda7870e2c23cd36e39d17f
Abdullah Khan <abdullahshamskhan@gmail.com>
2021-10-26 01:10:12 +0000

    Custom assertion function for checking equality

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("something", "something");
+assertEqual(1, 1);
+assertEqual(4, 6);
