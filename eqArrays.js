7cea40c744855de588d46c98c631d4b4031e3fa7
Abdullah Khan <abdullahshamskhan@gmail.com>
2021-10-27 18:31:41 +0000

    function which check the equality of two arrays

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const eqArrays = function(arr, arr2) {
+  // Edge case: Not same # of elements
+  if (arr.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  // Looped through entire array, comaparing every element
+  return true;
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+// TESTING
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
