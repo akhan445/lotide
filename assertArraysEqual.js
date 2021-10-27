8c9690465450f55ce4963de3f72b00ff5bfa15e0
Abdullah Khan <abdullahshamskhan@gmail.com>
2021-10-27 21:35:02 +0000

    added the assertion function for comparing equality of two arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+const assertArraysEqual = function(actualArray, expectedArray) {
+  const equalArrays = eqArrays(actualArray, expectedArray);
+
+  if (equalArrays) {
+    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(actualArray)} === ${JSON.stringify(expectedArray)}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${JSON.stringify(actualArray)} !== ${JSON.stringify(expectedArray)}`);
+
+  }
+}
+
+const eqArrays = function(arr, arr2) {
+  // Edge case: Not same # of elements
+  if (arr.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  // Looped through entire array, comaparing every element
+  return true;
+};
+
+// TESTING
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
